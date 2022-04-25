094f71f67c66bc989b310c8200ce2bd889e76e64
Jaime <1261360793@qq.com>
2022-04-25 18:18:21 +0800

    SVD

--- SVD/(sklearn)truncatedSVD.py
+++ SVD/(sklearn)truncatedSVD.py
@@ -1,9 +1,9 @@
-# 截断奇异值分解
-
 from sklearn.decomposition import TruncatedSVD
-# 导入Scipy生成稀疏数据模块
 from scipy.sparse import random as sparse_random
 
-# 创建稀疏数据X
-X = sparse_random(10,10,density=0.01,format='csr',random_state=42)
-print(X)
+# 创建100x100的稀疏矩阵(其中有100个取值在0-1范围内的非零元素)
+X = sparse_random(100,100,density=0.01,format='csr',random_state=42)
+# 基于截断SVD算法对X进行降维，降维的维度为5，即输出前5个奇异值
+svd = TruncatedSVD(n_components=5,n_iter=7,random_state=42)
+svd.fit(X)
+print(svd.singular_values_)
